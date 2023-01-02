229390f581ffbf4a576a1b1c075c959bfeb967ca
alexkols <kolosoff.alex@gmail.com>
2023-01-02 15:41:57 +0200

    Add alpha status to front-end

--- SymBufFront/src/app/components/buffer/load-buffer/load-buffer.component.ts
+++ SymBufFront/src/app/components/buffer/load-buffer/load-buffer.component.ts
@@ -9,12 +9,15 @@ import {FileService} from "../../../services/file.service";
 export class LoadBufferComponent {
 
   isLoading: boolean = false;
+
+  status: string = "Start position";
   constructor(private fileService: FileService) {
   }
   upload(event:any){
     this.isLoading = true;
     this.fileService.uploadFile(event.target.files[0]).subscribe(data => {
       console.log(data);
+      this.status = data;
      this.isLoading = false;
     }, error => {
       this.isLoading = false;
