1937a6e2e7754d14401d6a5306dd913210dfa01d
vbrizitschi <vbrizitschi@outlook.com>
2023-01-09 22:46:55 +0200

    corse enable :)

--- src/main/java/md/felicia/symphomybufferprep/config/SecurityConfig.java
+++ src/main/java/md/felicia/symphomybufferprep/config/SecurityConfig.java
@@ -11,13 +11,19 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -46,14 +52,13 @@ public class SecurityConfig  {
 
         http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
 
-        http = http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+       http = http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Token");
         }).and();
 
-        http    .csrf()
+        http.csrf()
                     .disable()
-                .cors()
-                    .disable()
+                .cors().and()
                 .exceptionHandling()
                     .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
