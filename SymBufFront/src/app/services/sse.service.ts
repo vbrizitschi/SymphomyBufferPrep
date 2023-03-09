9acd9b78492a8181e920474ce0c33293d7c1c744
alexkols <kolosoff.alex@gmail.com>
2023-03-09 15:56:20 +0200

    Modified SSE Service

--- SymBufFront/src/app/services/sse.service.ts
+++ SymBufFront/src/app/services/sse.service.ts
@@ -13,7 +13,7 @@ export class SseService {
   constructor( private zone: NgZone,
                private storageService: StorageService) { }
 
-  public getServerSentEvent(url: string, data: FormData): Observable<any> {
+  public getServerSentEvent(url: string, data: FormData|any): Observable<any> {
     return Observable.create((observer:any) => {
       const eventSource = this.getEventSourceWithPost(url, data);
       // Launch query
@@ -39,8 +39,56 @@ export class SseService {
     });
   }
 
+  public getServerSentEventJSON(url: string, data: any): Observable<any>{
+    return Observable.create(
+      (observer:any) => {
+        const eventSource = this.getEventSourceWithPost(url, data);
+        // Launch query
+        eventSource.stream();
+        // on answer from message listener
+        eventSource.onmessage = (event:any) => {
+          this.zone.run(() => {
+            observer.next(event.data);
+          });
+        };
+        eventSource.onerror = (error:any) => {
+          this.zone.run(() => {
+            observer.error(error);
+            this.closeEventSource();
+          });
+        };
+        eventSource.oncomplete = () => {
+          this.zone.run(() => {
+            observer.complete();
+            this.closeEventSource();
+          })
+        }
+      }
+    );
+  }
+
   private getEventSourceWithPost(url:string, formData:FormData) {
     return this.buildEventSource(url,'POST', formData);
+  }
+
+  private getEventSourceWithPostJSON(url: string, data: any) {
+    return  this.buildEventSourceJSON(url,'POST', data);
+  }
+
+  private buildEventSourceJSON(url: string, meth: string, data: any) {
+    const options = this.buildOptionsJSON(meth, data);
+    this.eventSource = new SSE(url, options);
+
+    // add listener
+    this.eventSource.addEventListener('message', (e:any) => {
+      return e.data;
+    });
+
+    this.eventSource.addEventListener('complete', (e:any) => {
+      return e;
+    })
+
+    return  this.eventSource;
   }
 
   private buildEventSource(url: string, meth: string, formData: FormData) {
@@ -80,6 +128,27 @@ export class SseService {
       payload: formData,
       method: meth,
       headers: {Authorization: 'Bearer '+token}
+    };
+  }
+
+  private buildOptionsJSON(
+    method: string,
+    data: any,
+  ): {
+    payload: any;
+    method: string;
+    headers: Headers;
+  } {
+
+    const token = this.storageService.getItem('token');
+    const headers: Headers;
+    headers.append('Authorization', 'Bearer ' + token);
+    headers.append('Content-Type', 'application/json')
+
+    return {
+      payload: data,
+      method: method,
+      headers: headers
     };
   }
 
